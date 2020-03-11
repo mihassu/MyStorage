df5ae52cc6c1bb4d40bf787272337c52c98df92c
mihassu <mihassu1@gmail.com>
2020-03-11 19:12:18 +0300

    Initial commit

---
+++ client/src/main/java/MainClient.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class MainClient {
+    public static void main(String[] args) {
+//        File fileClient = new File("client-storage/1.txt");
+//        File fileServer = new File("server-storage/2.txt");
+//        int i;
+//
+//        try(FileInputStream finClient = new FileInputStream(fileClient);
+//            RandomAccessFile out = new RandomAccessFile(fileServer, "rw");
+//
+//        ) {
+//            
+//            while (true) {
+//                i = finClient.read();
+//                if (i == -1) break;
+//                out.write(i);
+//            }
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+    }
+}
